0872633bf2f371e3a5ffe25e8bf569bd9ba613ed
Renata Santana <renatasantana114@gmail.com>
2022-04-12 17:08:02 -0300

    e

--- sql/criar_tabela_categorias.sql
+++ sql/criar_tabela_categorias.sql
@@ -1,8 +1,1 @@
 SELECT 'SUBTITUA O CONTEÚDO DESTE ARQUIVO PELA SUA RESPOSTA :-)';
-CREATE TABLE Categorias(
-	ID int identity primary key,
-    NOME text not null,
-    DESCONTO decimal
-);
-
-SELECT Categorias
